3fe541b2b9d11fc388c0cf2bb8e4f6780eda2536
FFakoBaba <102943980+FFakoBaba@users.noreply.github.com>
2022-04-04 02:07:14 -0400

    Create user.js

---
+++ user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         Bayrak şablonu
+// @namespace    http://tampermonkey.net/
+// @version      0.31
+// @description  elraenin bayrağı
+// @author       oralekin, FFakoBaba
+// @match        https://hot-potato.reddit.com/embed*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
+// @grant        none
+// ==/UserScript==
+if (window.top !== window.self) {
+    window.addEventListener('load', () => {
+            document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
+        (function () {
+            const i = document.createElement("img");
+            i.src = "https://github.com/FFakoBaba/ffakobaba.github.io/raw/main/overlay.png";
+            i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;z-index:31;";
+            console.log(i);
+            return i;
+        })())
+
+    }, false);
+}
